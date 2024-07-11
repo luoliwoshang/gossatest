5795c0eae7ea33edac21b0953dffb05583d7aa33
luoliwoshang <2643523683@qq.com>
2024-07-11 14:43:43 +0800

    error:依赖fmt未构建

--- ssa_demo.go
+++ ssa_demo.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"golang.org/x/tools/go/packages"
+	"golang.org/x/tools/go/ssa"
+	"golang.org/x/tools/go/ssa/ssautil"
 )
 
 func main() {
@@ -21,8 +24,22 @@ func main() {
 		log.Fatalf("No packages found.")
 	}
 
-	// 打印包中的所有Go文件
-	for _, pkg := range pkgs {
-		fmt.Printf("Package: %s, Files: %v\n", pkg.PkgPath, pkg.GoFiles)
+	// Create an SSA program from the loaded packages
+	ssaProg, ssaPkgs := ssautil.Packages(pkgs, ssa.BuilderMode(ssa.SanityCheckFunctions))
+	ssaProg.Build()
+
+	//error: panic: Package("test/im").Build(): unsatisfied import: Program.CreatePackage("fmt") was not called
+
+	// 打印每个包中的函数
+	for _, ssaPkg := range ssaPkgs {
+		if ssaPkg != nil {
+			ssaPkg.Build()
+			for _, member := range ssaPkg.Members {
+				if fn, ok := member.(*ssa.Function); ok {
+					fmt.Fprintln(os.Stdout, fn.String())
+					fn.WriteTo(os.Stdout)
+				}
+			}
+		}
 	}
 }
