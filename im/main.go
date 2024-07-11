9ef9f1444301cc3b8f2e68feaef59781f134c99e
luoliwoshang <2643523683@qq.com>
2024-07-11 14:56:36 +0800

    main func

--- im/main.go
+++ im/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
+
+type Sockaddr_in struct {
+	Sin_len    uint8
+	Sin_family uint8
+	Sin_port   uint16
+	Sin_addr   In_addr
+	Sin_zero   [8]int8
+}
+
+type In_addr struct {
+	S_addr uint8
+}
 
 func main() {
-	fmt.Println("Hello, SSA!")
+	fmt.Println(unsafe.Sizeof(Sockaddr_in{}))
 }
--- ssa_demo.go
+++ ssa_demo.go
@@ -12,11 +12,12 @@ import (
 
 func main() {
 	cfg := &packages.Config{
-		Mode: packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles | packages.NeedTypes | packages.NeedSyntax | packages.NeedTypesInfo,
+		Mode: packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles | packages.NeedTypes | packages.NeedSyntax | packages.NeedTypesInfo | packages.NeedImports,
 		Dir:  ".", // 确保这是项目根目录
 	}
 
-	pkgs, err := packages.Load(cfg, "test/im")
+	// 加载包和它的依赖
+	pkgs, err := packages.Load(cfg, "test/im", "fmt")
 	if err != nil {
 		log.Fatalf("Failed to load packages: %s", err)
 	}
@@ -24,18 +25,16 @@ func main() {
 		log.Fatalf("No packages found.")
 	}
 
-	// Create an SSA program from the loaded packages
-	ssaProg, ssaPkgs := ssautil.Packages(pkgs, ssa.BuilderMode(ssa.SanityCheckFunctions))
+	// 创建 SSA 程序
+	ssaProg, ssaPkgs := ssautil.AllPackages(pkgs, ssa.BuilderMode(ssa.SanityCheckFunctions))
 	ssaProg.Build()
-
-	//error: panic: Package("test/im").Build(): unsatisfied import: Program.CreatePackage("fmt") was not called
 
 	// 打印每个包中的函数
 	for _, ssaPkg := range ssaPkgs {
 		if ssaPkg != nil {
 			ssaPkg.Build()
 			for _, member := range ssaPkg.Members {
-				if fn, ok := member.(*ssa.Function); ok {
+				if fn, ok := member.(*ssa.Function); ok && fn.Name() == "main" {
 					fmt.Fprintln(os.Stdout, fn.String())
 					fn.WriteTo(os.Stdout)
 				}
